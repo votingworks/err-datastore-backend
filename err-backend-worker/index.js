b82efe59995afce05d02c4522ccbc98945477404
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-05-25 09:17:11 -0700

    style: `npm run format`

--- err-backend-worker/index.js
+++ err-backend-worker/index.js
@@ -1,4 +1,3 @@
-
 // the normal Cloudflare worker HTTP <--> event handler hook
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -6,36 +5,36 @@ addEventListener('fetch', event => {
 
 // all HTTP requests go here
 async function handleRequest(request) {
-  const publicURLToken = await ERR.get("publicURLToken")
-  const secretURLToken = await ERR.get("secretURLToken")
+  const publicURLToken = await ERR.get('publicURLToken')
+  const secretURLToken = await ERR.get('secretURLToken')
 
   const url = new URL(request.url)
 
-  if (request.method === "GET" && url.pathname === "/" + publicURLToken) {
-    return new Response(await ERR.get("content"), {
-	headers: {
-	    'content-type': 'application/json',
-	    'Access-Control-Allow-Origin': '*'
-	},
+  if (request.method === 'GET' && url.pathname === '/' + publicURLToken) {
+    return new Response(await ERR.get('content'), {
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
     })
   }
 
-  if (request.method === "POST" && url.pathname === "/" + secretURLToken) {
+  if (request.method === 'POST' && url.pathname === '/' + secretURLToken) {
     // try to parse JSON body
     const json_body = await request.json()
 
     // if succeeds, store it
     if (json_body) {
-      await ERR.put("content", JSON.stringify(json_body))
-      return new Response("ok", {
-	headers: { 'content-type': 'text/plain' },
+      await ERR.put('content', JSON.stringify(json_body))
+      return new Response('ok', {
+        headers: { 'content-type': 'text/plain' },
       })
     } else {
-      return new Response("no JSON content", {
-	headers: { 'content-type': 'text/plain' },
+      return new Response('no JSON content', {
+        headers: { 'content-type': 'text/plain' },
       })
     }
   }
-  
-  return new Response("nothing here yet.")
+
+  return new Response('nothing here yet.')
 }
